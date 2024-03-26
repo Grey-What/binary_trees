301fe9a7358e635146485f7ae01914fc3d8a977f
Grey-What <greywhat2002@gmail.com>
2024-03-26 06:16:27 -0700

    betty fix

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 /**
- * binary_tree_node: creates a binary tree node
+ * binary_tree_node - creates a binary tree node
  *
  * @parent: pointer to parent node
  * @value: value to be sotred in node
  *
- * Retrun: pointer to new node, else NULL
+ * Return: pointer to new node, else NULL
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
